1f64de5aff059db4dab0cac45cbe58d45132ffbc
Matheus Santos <somathsant@gmail.com>
2025-08-08 12:05:48 -0300

    feat: add category page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,5 +1,6 @@
 import { categoryTable, productTable } from "@/db/schema";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 interface CategorySelectorProps {
   categories: (typeof categoryTable.$inferSelect)[];
@@ -15,7 +16,7 @@ const CategorySelector = ({ categories }: CategorySelectorProps) => {
             variant="ghost"
             className="rounded-full bg-white text-xs font-semibold"
           >
-            {category.name}
+            <Link href={`/category/${category.slug}`}>{category.name}</Link>
           </Button>
         ))}
       </div>
--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -1,5 +1,6 @@
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { format } from "path";
@@ -8,9 +9,10 @@ interface ProductItemProps {
   product: typeof productTable.$inferSelect & {
     variants: (typeof productVariantTable.$inferSelect)[];
   };
+  textContainerClassName?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
   const firstVariant = product.variants[0];
 
   return (
@@ -18,11 +20,17 @@ const ProductItem = ({ product }: ProductItemProps) => {
       <Image
         src={firstVariant.imageUrl}
         alt={firstVariant.name}
-        height={200}
-        width={200}
-        className="rounded-3xl"
+        sizes="100vw"
+        height={0}
+        width={0}
+        className="h-auto w-full rounded-3xl"
       />
-      <div className="flex max-w-[200px] flex-col gap-1">
+      <div
+        className={cn(
+          "flex max-w-[200px] flex-col gap-1",
+          textContainerClassName,
+        )}
+      >
         <p className="truncate text-sm font-medium">{product.name}</p>
         <p className="text-muted-foreground truncate text-xs font-medium">
           {product.description}
