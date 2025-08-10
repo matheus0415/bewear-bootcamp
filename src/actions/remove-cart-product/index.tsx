ad4f1f0cadc86c49d9495538ef4f96fe32d178ce
Matheus Santos <somathsant@gmail.com>
2025-08-10 15:56:48 -0300

    feat: ensure product can be removed from cart

---
+++ src/actions/remove-cart-product/index.tsx
@@ -0,0 +1,41 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+
+import { headers } from "next/headers";
+import { db } from "@/db";
+import { cartItemTable, cartTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { removeProductFromCartSchema } from "./schema";
+import z from "zod";
+
+export const removeProductFromCart = async (data: z.infer<typeof removeProductFromCartSchema>) => {
+  removeProductFromCartSchema.safeParse(data);
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    throw new Error("User not authenticated");
+  }
+  
+  const cartItem = await db.query.cartItemTable.findFirst({
+    where: (cartItem, { eq }) =>
+      eq(cartItem.id, data.cartItemId),
+    with: {
+        cart: true,
+    },
+  });
+
+  const cartDoesNotBelongToUser = cartItem?.cart.userId !== session.user.id;
+
+  if (cartDoesNotBelongToUser) {
+    throw new Error("User not authorized to remove this item");
+  }
+
+  if (!cartItem) {
+    throw new Error("Product variant not found in cart");
+  }
+  await db.delete(cartItemTable).where(eq(cartItemTable.id, cartItem.id));
+};
--- src/components/common/cart-item.tsx
+++ src/components/common/cart-item.tsx
@@ -1,8 +1,10 @@
+import { removeProductFromCart } from "@/actions/remove-cart-product";
+import { formatCentsToBRL } from "@/helpers/money";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
 import Image from "next/image";
 import { Button } from "../ui/button";
-import { format } from "path";
-import { formatCentsToBRL } from "@/helpers/money";
+import { toast } from "sonner";
 
 interface CartItemProps {
   id: string;
@@ -21,6 +23,28 @@ const CartItem = ({
   productVariantPriceInCents,
   quantity,
 }: CartItemProps) => {
+
+  const queryClient = useQueryClient();
+
+  const removeProductFromCartMutation = useMutation({
+    mutationKey: ["remove-from-cart"],
+    mutationFn: () => removeProductFromCart({ cartItemId: id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["cart"] });
+    }
+  });
+
+  const handleDeleteClick = () => {
+    removeProductFromCartMutation.mutate(undefined, {
+      onSuccess: () => {
+        toast.success("Produto removido do carrinho.")
+      },
+      onError: (error) => {
+        toast.error("Erro ao remover produto do carrinho.")
+      },
+    });
+  };
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-4">
@@ -49,7 +73,7 @@ const CartItem = ({
         </div>
       </div>
       <div className="flex flex-col items-end justify-center gap-2">
-        <Button variant={"outline"} size="icon">
+        <Button variant={"outline"} size="icon" onClick={handleDeleteClick}>
           <TrashIcon />
         </Button>
         <p className="text-sm font-semibold">
